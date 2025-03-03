502a816e167c674dabea093b0e1b4a10220f7760
hreshigit <hkad.236@gmail.com>
2025-03-03 04:24:01 +0000

    changed second commit error

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
 public class hello{
     public static void main(String[] args) {
         String name = "hreshi";
-        // System.out.println(name.charAt(0));
-        sout.println(name.substring(0,3));
+        System.out.println(name.charAt(0));
+        System.out.println(name.substring(0,3));
         System.out.println(name.substring(3));
     }
 }
