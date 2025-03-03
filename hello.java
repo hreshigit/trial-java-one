93c5dff0ca22f9ed09b94f7048603714ec7043de
hreshigit <hkad.236@gmail.com>
2025-03-03 04:22:17 +0000

    second commit

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class hello{
     public static void main(String[] args) {
         String name = "hreshi";
         // System.out.println(name.charAt(0));
-        
+        sout.println(name.substring(0,3));
+        System.out.println(name.substring(3));
     }
 }
