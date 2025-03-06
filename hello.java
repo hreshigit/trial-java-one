ec9247affda68d81563af96c4384179f2bdf20ed
hreshigit <hkad.236@gmail.com>
2025-03-06 11:44:46 +0000

    for loop added. lol

--- hello.java
+++ hello.java
@@ -1,8 +1,16 @@
 public class hello{
     public static void main(String[] args) {
-        String name = "hreshi";
-        System.out.println(name.charAt(0));
-        System.out.println(name.substring(0,3));
-        System.out.println(name.substring(3));
+        // String name = "hreshi";
+        // System.out.println(name);
+        // System.out.println(name.length());
+        // System.out.println(name.toUpperCase());
+        // System.out.println(name.toLowerCase());
+        // System.out.println(name.indexOf('h'));
+ 
+        int[] a = {4,3,2,1};
+        for ( int i=a.length-1; i>=0; i=i-1){
+            System.out.println(a[i]);
+        }
+        
     }
 }
