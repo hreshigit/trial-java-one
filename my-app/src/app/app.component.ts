dac09e05399ecda446de48c4e1233b898da7daa5
hreshigit <hkad.236@gmail.com>
2025-04-18 07:32:12 +0000

    portfolio skeleton

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet,RouterModule } from '@angular/router';
 import { AboutComponent } from './myComponents/about/about.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './myComponents/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet,RouterModule,AboutComponent,FormsModule],
+  imports: [RouterOutlet,RouterModule,AboutComponent,HomeComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 
 export class AppComponent {
   title = 'my-app';
--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './myComponents/about/about.component';
 import { ContactComponent } from './myComponents/contact/contact.component';
+import { HomeComponent } from './myComponents/home/home.component';
+import { ProjectsComponent } from './myComponents/projects/projects.component';
 
 export const routes: Routes = [
             {path:'about',component:AboutComponent},
-            {path:'contact',component:ContactComponent}
+            {path:'contact',component:ContactComponent},
+            {path:'home',component:HomeComponent},
+            {path:'projects',component:ProjectsComponent}
 ];
